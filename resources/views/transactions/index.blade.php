3389039052b5e5500689d4a84223b21fa4fe4e78
aga <105931184+AgaHibatulloh@users.noreply.github.com>
2024-12-26 10:52:59 +0700

    Update Design Table

--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -9,41 +9,43 @@
         <a href="{{ route('transactions.export-pdf') }}" class="btn btn-secondary mb-3">Export to PDF</a>
     @endif
 
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>User</th>
-                <th>Bid</th>
-                <th>Nominal</th>
-                <th>Image</th>
-                <th>Status</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($transactions as $transaction)
+    <div class="table-responsive">
+        <table class="table table-striped table-hover table-bordered">
+            <thead class="thead-dark">
                 <tr>
-                    <td>{{ $transaction->id }}</td>
-                    <td>{{ $transaction->user->name }}</td>
-                    <td>{{ $transaction->bid->id }}</td>
-                    <td>{{ $transaction->nominal }}</td>
-                    <td>
-                        <img src="{{ asset('storage/' . $transaction->image) }}" alt="Image" style="width: 100px;">
-                    </td>
-                    <td>{{ ucfirst($transaction->status) }}</td>
-                    <td>
-                        <a href="{{ route('transactions.show', $transaction->id) }}" class="btn btn-info btn-sm">View</a>
-                        <a href="{{ route('transactions.edit', $transaction->id) }}" class="btn btn-warning btn-sm">Edit</a>
-                        <form action="{{ route('transactions.destroy', $transaction->id) }}" method="POST" style="display:inline;">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger btn-sm">Delete</button>
-                        </form>
-                    </td>
+                    <th>ID</th>
+                    <th>User</th>
+                    <th>Bid</th>
+                    <th>Nominal</th>
+                    <th>Image</th>
+                    <th>Status</th>
+                    <th>Actions</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($transactions as $transaction)
+                    <tr>
+                        <td>{{ $transaction->id }}</td>
+                        <td>{{ $transaction->user->name }}</td>
+                        <td>{{ $transaction->bid->id }}</td>
+                        <td>{{ $transaction->nominal }}</td>
+                        <td>
+                            <img src="{{ asset('storage/' . $transaction->image) }}" alt="Image" class="img-thumbnail" style="width: 100px;">
+                        </td>
+                        <td>{{ ucfirst($transaction->status) }}</td>
+                        <td>
+                            <a href="{{ route('transactions.show', $transaction->id) }}" class="btn btn-info btn-sm">View</a>
+                            <a href="{{ route('transactions.edit', $transaction->id) }}" class="btn btn-warning btn-sm">Edit</a>
+                            <form action="{{ route('transactions.destroy', $transaction->id) }}" method="POST" style="display:inline;">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
 @endsection
